0bea5e2e5f2ee6a13b8e0c8df9c432a41eef3360
SarahIsWeird <morningcrafter0603@gmail.com>
2022-09-11 14:54:53 +0200

    Initial Commit

---
+++ HWR ILE/HWR_ILEApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWR_ILEApp.swift
+//  HWR ILE
+//
+//  Created by Sarah Klocke on 11.09.22.
+//
+
+import SwiftUI
+
+@main
+struct HWR_ILEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
